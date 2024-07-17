3f04c727599d8666b1799464d4e0f5e7d5a7d640
wilbur-z <zhengwei101@aliyun.com>
2024-07-17 09:47:10 +0800

    [MDF] add test for insertion sort

--- 算法/排序算法/选择排序.cpp
+++ 算法/排序算法/选择排序.cpp
@@ -1,20 +1,6 @@
 #include "SortTestHelper.h"
 #include "Student.h"
-
-template <typename T>
-void selectionSort(T arr[], int n) {
-    for (int i = 0; i < n; i++) {
-        // 寻找[i,n)区间里的最小值
-        int minIndex = i;
-        for (int j = i + 1; j < n; j++) {
-            if (arr[j] < arr[minIndex]) {
-                minIndex = j;
-            }
-        }
-        std::swap(arr[i], arr[minIndex]);
-    }
-}
-
+#include "SelectionSort.h"
 
 int main() {
     int n = 10000;
@@ -34,6 +20,9 @@ int main() {
 
     Student d[4] = {{"D", 90}, {"A", 100}, {"C", 95}, {"B", 95}};
     selectionSort(d, 4);
-    for (auto& i : d)
+    for (auto& i : d) {
         cout << i;
+    }
+       
+    return 0;
 }
