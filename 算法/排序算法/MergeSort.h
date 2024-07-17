75772bac038520ff67566bfe6892b98d7c3d7db3
wilbur-z <zhengwei101@aliyun.com>
2024-07-17 11:41:09 +0800

    [ADD] merge sort implementation

---
+++ 算法/排序算法/MergeSort.h
@@ -0,0 +1,56 @@
+#pragma once
+
+//将arr[l ... mid] 和 arr[mid+1 ... r]两部分进行归并
+template <typename T>
+void __merge(T arr[], int l, int mid, int r) {
+    //先定义一个和arr相同大小的辅助空间
+    T* aux = new T[r - l + 1];
+
+    for (int i = l; i <= r; i++) {
+        aux[i - l] = arr[i];
+    }
+
+    //初始化，i指向左半部分的起始索引值l；j指向右半部分起始索引位置mid+1
+    int i = l;
+    int j = mid + 1;
+    for (int k = l; k <= r; k++) {
+        if (i <= mid && j <= r) { //两数组均未越界
+            // 左半部分所指元素 < 右半部分所指元素
+            if (aux[i - l] < aux[j - l]) {
+                arr[k] = aux[i - l];
+                i++;
+            }
+            else { // 左半部分所指元素 >= 右半部分所指元素
+                arr[k] = aux[j - l];
+                j++;
+            }
+        }
+        else if (i > mid) { // 如果左半部分元素已经全部处理完毕
+            arr[k] = aux[j - l];
+            j++;
+        }
+        else if (j > r) { // 如果右半部分元素已经全部处理完毕
+            arr[k] = aux[i - l];
+            i++;
+        }
+    }
+
+    delete[] aux;
+}
+
+// 递归使用归并排序，对arr[l ... r]的范围进行排序
+template <typename T>
+void __mergeSort(T arr[], int l, int r) {
+    if (l >= r) {
+        return;
+    }
+    int mid = l + (r - l) / 2;
+    __mergeSort(arr, l, mid);
+    __mergeSort(arr, mid + 1, r);
+    __merge(arr, l, mid, r);
+}
+
+template <typename T>
+void mergeSort(T arr[], int n) {
+    __mergeSort(arr, 0, n - 1);
+}
---
+++ 算法/排序算法/归并排序.cpp
@@ -0,0 +1,24 @@
+#include "SortTestHelper.h"
+#include "InsertionSort.h"
+#include "MergeSort.h"
+
+int main() {
+    // Merge Sort是我们学习的第一个O(nlogn)复杂度的算法
+    // 可以在1秒之内轻松处理100万数量级的数据
+    // 注意：不要轻易尝试使用SelectionSort, InsertionSort或者BubbleSort处理100万级的数据
+    // 否则，你就见识了O(n^2)的算法和O(nlogn)算法的本质差异：）
+    int n = 50000;
+
+    // 测试1 一般性测试
+    cout << "Test for random array, size = " << n << ", random range [0, " << n << "]" << endl;
+    int* arr1 = SortTestHelper::generateRandomArray(n, 0, n);
+    int* arr2 = SortTestHelper::copyIntArray(arr1, n);
+
+    SortTestHelper::testSort("Insertion Sort", insertionSort, arr1, n);
+    SortTestHelper::testSort("Merge Sort", mergeSort, arr2, n);
+
+    delete[] arr1;
+    delete[] arr2;
+
+    cout << endl;
+}
